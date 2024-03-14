a401f59792c0e8cc7d6e36891c057261c587563c
Jake-Andrews <jake.andrews1@ontariotechu.net>
2024-03-14 17:21:52 -0400

    basic project structure, no code yet

---
+++ cmd/web_dl/main.go
@@ -0,0 +1,40 @@
+package main
+
+/*
+Future options:
+	History
+	Validate URL given by user (github.com/asaskevich/govalidator govalidator.IsURL(url))
+	Duplicate check
+	Auth
+	Options (proxy, timeouts, etc...)
+	Dockerize
+	Github workflow
+	Display progress 
+
+Current Goals:
+	CLI
+	Requests
+	Save data
+	Logging
+	Concurency
+
+	Proper Tests
+	HEAD request and deal with range response
+	More specificity when dealing with servers responses
+
+Current To Do Order:
+	cmd/web_dl/main.go
+	client.go
+	requests.go
+	download.go
+	util.go
+
+	1. Create a CLI, option to pass URL
+	2. Perform GET request on URL passed through/save data
+	4. Logging
+	5. Concurency
+*/
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Jake-Andrews/web_dl
+
+go 1.22.0
